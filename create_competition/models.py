08769da47f441ae77720d2583d635e95f9d94f78
hygorrasec <coehmaneh@gmail.com>
2022-07-17 00:28:05 -0300

    Created first migrate for competitions

--- create_competition/models.py
+++ create_competition/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class CreateCompetition(models.Model):
+
+    name_competition = models.CharField(max_length=255)
+    place_competition = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    name_athlete = models.CharField(max_length=255)
+    age_athlete = models.CharField(max_length=25)
+    height_athlete = models.CharField(max_length=25)
+    weight_athlete = models.CharField(max_length=25)
+
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_competition
