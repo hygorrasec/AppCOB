e335b84a9ea5da443c0908a79fb7d8a92ee07e74
Hygor Rasec <hygorrasec@gmail.com>
2022-07-22 16:05:24 -0300

    Testing in views.py

--- create_competition/views.py
+++ create_competition/views.py
@@ -1,7 +1,9 @@
+from distutils.ccompiler import new_compiler
+
 from django.shortcuts import render
 
 from .forms import CreateForm
-from .models import CreateCompetition
+from .models import Competition, CreateCompetition
 
 
 def create_competition(request):
@@ -25,7 +27,47 @@ def create_competition(request):
     else:
         # form = CreateForm()
         # print(f'FORM: {form}')
-        getAthletes = CreateCompetition.objects.all()  # .order_by('-created_at')
+
+        # Criando nova competição como teste (modo 1):
+        # new_competition = Competition()
+        # new_competition.competition = 'teste'
+        # new_competition.save()
+
+        # Criando nova competição como teste (modo 2):
+        # new_competition = Competition.objects.create(
+        #     competition='Isso foi criado via objects.create')
+
+        # Atualizando uma competição:
+        # new_competition = Competition.objects.create(
+        #     competition='Isso foi criado via objects.create para ser alterado.')
+        # print(
+        #     f'Imprimindo para verificar a informação antes de ser alterada: {new_competition}')
+        # new_competition.competition = 'TEXTO ALTERADO!!!!!!!!'
+        # new_competition.save()
+
+        # Buscando uma competição:
+        # outra_competition = Competition()
+        # outra_competition.competition = 'Outra competição!!!'
+        # outra_competition.save()
+        # competicao = Competition.objects.get(id=19)
+        # print(f'COMPETIÇÃO ENCONTRADA: {competicao}')
+
+        # Deletando do banco:
+        # outra_competition2 = Competition()
+        # outra_competition2.competition = 'Acabou de criar e será deletado.'
+        # outra_competition2.save()
+        # print(outra_competition2)
+        # outra_competition2.delete()
+        # print('Foi deletado!')
+        # print(outra_competition2)
+
+        # Filtrando e deletando uma competição: (importante usar o filter no lugar do get pois não apresenta erro)
+        # competition_filter = Competition.objects.filter(id=21)
+        # print(f'ENCONTROU: {competition_filter}')
+        # competition_filter.first()
+        # competition_filter.first().delete()
+
+        getAthletes = CreateCompetition.objects.filter().order_by('-id')
 
         count_1 = 0
         count_2 = 0
